ba35d7ce41bf3665288ce844bd15d18fb7682e29
baddabudda <radkova.irene@gmail.com>
2025-07-01 00:28:28 +0300

    refactor: move "delete all" to bottom

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,7 @@ function changeMode() {
 <template>
   <ToolbarComponent :editModeEnabled @on-delete-all="deleteAll" @on-change-mode="changeMode"/>
   <div class="main-container">
-    <button v-if="editModeEnabled" class="delete-button" @click="deleteAll">Delete all</button>
+    <!-- <button v-if="editModeEnabled" class="delete-button" @click="deleteAll">Delete all</button> -->
     <div class="normal-width container-list">
       <ItemComponent
         v-for="item in items"
@@ -83,7 +83,7 @@ function changeMode() {
       />
     </div>
   </div>
-  <BottomBar @on-submit="handleSubmit" />
+  <BottomBar :editModeEnabled @on-submit="handleSubmit" @on-delete-all="deleteAll"/>
 </template>
 
 <style scoped>
